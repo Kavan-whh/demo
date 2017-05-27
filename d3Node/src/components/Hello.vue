60614b23ed08c948430d6620d4e07d96279109e4
稻草人 <462001808@qq.com>
2017-05-27 10:44:49 +0800

    dd

--- d3Node/src/components/Hello.vue
+++ d3Node/src/components/Hello.vue
@@ -20,8 +20,8 @@ export default {
     var circles = d3.select('#circles') //圆形
     var polygons = d3.select('#polygons') //三角
 
-// 坐标比例尺
-    var dataset = [1, 2, 3,1000];
+    // 坐标比例尺
+    var dataset = [1, 2, 3, 1000];
     var yScale = d3.scaleBand() //序数比例尺
       .domain(d3.range(dataset.length)) //    设置序数比例尺的值域
       .range([0, 1000]); //根据输入值域的长度将自动将其分为相等的块，并且设定间距
@@ -30,48 +30,53 @@ export default {
       .domain([0, d3.max(dataset)]) //设置线性比例尺的值域
       .range([0, 950]); //设置线性比例尺的输出范围
 
-      var xAxis=d3.axisTop(xScale)
-      var yAxis=d3.axisLeft(yScale)
-      // xAxis(chart)
-      // yAxis(chart)
-      chart.append("g")
-      .attr("transform","translate(30,20)")
+    var xAxis = d3.axisTop(xScale)
+    var yAxis = d3.axisLeft(yScale)
+    // xAxis(chart)
+    // yAxis(chart)
+    chart.append("g")
+      .attr("transform", "translate(30,20)")
       .call(xAxis)
 
-      chart.append("g")
-      .attr("transform","translate(30,50)")
+    chart.append("g")
+      .attr("transform", "translate(30,50)")
       .call(yAxis)
 
-// 文本块
-      chart.selectAll('text')
-      .attr("y", 0)//文本坐标
+    // 文本块
+    chart.selectAll('text')
+      .attr("y", 0) //文本坐标
       .attr("x", 8)
-      .attr("dy", "2em")//移动的坐标
+      .attr("dy", "2em") //移动的坐标
 
 
 
 
 
 
-    var drag1 = d3.drag()//多边形拖拽
+    var drag1 = d3.drag() //多边形拖拽
       .on("drag", function(d, i) {
         d3.select(this).attr("transform", function(d, i) {
-          return "translate(" + [d3.event.x-200, d3.event.y-100] + ")"
+          return "translate(" + [d3.event.x - 200, d3.event.y - 100] + ")"
         })
       });
 
-    var drag2 = d3.drag()//矩形拖拽
+    var drag2 = d3.drag() //矩形拖拽
       .on("drag", function(d, i) {
         d3.select(this)
-          .attr("x", d3.event.x-25)//定到拖拽中间
-          .attr("y", d3.event.y-25)
+          .attr("x", d3.event.x - 25) //定到拖拽中间
+          .attr("y", d3.event.y - 25)
 
       })
-    var drag3 = d3.drag()//圆形拖拽
+    var drag3 = d3.drag() //圆形拖拽
       .on("drag", function(d, i) {
         d3.select(this)
           .attr("cx", d3.event.x)
-          .attr("cy", d3.event.y);
+          .attr("cy", d3.event.y)
+          .style("fill", "yellow");
+      })
+      .on('end', function() {
+        d3.select(this)
+          .style('fill', 'green')
       });
 
     rects.call(drag2);
