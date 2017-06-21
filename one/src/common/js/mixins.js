79a9c62a2eb5b3683d90b0495796e1fc09279419
稻草人 <462001808@qq.com>
2017-06-21 17:30:11 +0800

    d

--- one/src/common/js/mixins.js
+++ one/src/common/js/mixins.js
@@ -7,6 +7,62 @@ const mixins = {
       temp = Object.assign(temp, obj)
       return temp
     }
+  },
+  mounted() {
+    var params = { // 容器
+      left: 0,
+      top: 0,
+      currentX: 0,
+      currentY: 0,
+      flag: false,
+      ele: null
+    }
+    const self = this
+    $('.huabu').delegate('.box', 'mousedown', function(event) {
+      params.ele = $(this)
+      const target = params.ele
+      const e = event
+      // 初始化
+      params.flag = true
+      if (self.getCss(target, 'left') !== 'auto') {
+        params.left = self.getCss(target, 'left')
+      }
+      if (self.getCss(target, 'top') !== 'auto') {
+        params.top = self.getCss(target, 'top')
+      }
+      params.currentX = e.clientX
+      params.currentY = e.clientY
+    })
+    $(document).delegate('.box', 'mouseup', function() {
+      const target = params.ele
+      if (self.getCss(target, 'left') !== 'auto') {
+        params.left = self.getCss(target, 'left')
+      }
+      if (self.getCss(target, 'top') !== 'auto') {
+        params.top = self.getCss(target, 'top')
+      }
+      params.flag = false
+      params.ele = null
+      console.log(params)
+    })
+    $('.huabu').mousemove(function() {
+      if (!params.flag) {
+        return
+      }
+      const target = params.ele
+      const e = event || window.event
+      const nowX = e.clientX
+      const nowY = e.clientY
+      const disX = nowX - params.currentX
+      const disY = nowY - params.currentY
+      target.css('left', parseInt(params.left) + disX + 'px')
+      target.css('top', parseInt(params.top) + disY + 'px')
+    })
+  },
+  methods: {
+    getCss(o, key) {
+      return o.css(key)
+    }
   }
 }
 export default mixins
--- one/src/components/input.vue
+++ one/src/components/input.vue
@@ -10,66 +10,7 @@
 import mixins from 'common/js/mixins'
 // import drage from 'common/js/drage'
 export default {
-  mixins: [mixins],
-  mounted() {
-    var params = { // 容器
-      left: 0,
-      top: 0,
-      currentX: 0,
-      currentY: 0,
-      flag: false,
-      ele: null
-    }
-    const self = this
-
-    $('.huabu').delegate('.box', 'mousedown', function(event) {
-      params.ele = $(this)
-      const target = params.ele
-      const e = event
-      // 初始化
-      params.flag = true
-      if (self.getCss(target, 'left') !== 'auto') {
-        params.left = self.getCss(target, 'left')
-      }
-      if (self.getCss(target, 'top') !== 'auto') {
-        params.top = self.getCss(target, 'top')
-      }
-      params.currentX = e.clientX
-      params.currentY = e.clientY
-    })
-    $(document).delegate('*', 'mouseup', function() {
-      // console.log(this)
-      const target = params.ele
-      if (self.getCss(target, 'left') !== 'auto') {
-        params.left = self.getCss(target, 'left')
-      }
-      if (self.getCss(target, 'top') !== 'auto') {
-        params.top = self.getCss(target, 'top')
-      }
-      console.log(params)
-      params.flag = false
-      params.ele = null
-    })
-    $('.huabu').mousemove(function() {
-      if (!params.flag) {
-        return
-      }
-      const target = params.ele
-      const e = event || window.event
-      const nowX = e.clientX
-      const nowY = e.clientY
-      const disX = nowX - params.currentX
-      const disY = nowY - params.currentY
-      console.log(disX, disY)
-      target.css('left', parseInt(params.left) + disX + 'px')
-      target.css('top', parseInt(params.top) + disY + 'px')
-    })
-  },
-  methods: {
-    getCss(o, key) {
-      return o.css(key)
-    }
-  }
+  mixins: [mixins]
 }
 </script>
 
