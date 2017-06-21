12ea7473f9b5b9fae412e28abe819712feac1bd5
稻草人 <462001808@qq.com>
2017-06-21 16:51:53 +0800

    d

--- one/src/components/input.vue
+++ one/src/components/input.vue
@@ -12,22 +12,47 @@ import drage from 'common/js/drage'
 export default {
   mixins: [mixins],
   mounted() {
-    const obj = document.getElementsByClassName('box')[0]
-    let cb = function(data) {
-      console.log(data)
+    const params = { // 容器
+      left: 0,
+      top: 0,
+      currentX: 0,
+      currentY: 0,
+      flag: false
     }
-    drage(obj, obj, cb)
-  },
-  methods: {
-    trigger() {
-      // const obj = e.target
-      // let cb = function(data) {
-      //   console.log(data)
-      // }
-      // this.drage(obj, cb)
-    }
+    $('.huabu').delegate('.box', 'mousedown', function(event) {
+      params.ele = this
+      params.flag = true
+      const e = event
+      params.currentX = e.clientX
+      params.currentY = e.clientY
+    })
+    // $(document).delegate(('*', 'mouseup', function() {
+    //   console.log(this)
+    //   params.flag = false
+    //   const target = $(this)
+    //   if (self.getCss(target, 'left') !== 'auto') {
+    //     params.left = self.getCss(target, 'left')
+    //   }
+    //   if (self.getCss(target, 'top') !== 'auto') {
+    //     params.top = self.getCss(target, 'top')
+    //   }
+    // })
+    $('.huabu').mousemove(function() {
+      if (!params.flag) {
+        return
+      }
+      // $(document).mousemove(function() {
+      const target = $(this)
+      const e = event || window.event
+      const nowX = e.clientX
+      const nowY = e.clientY
+      const disX = nowX - params.currentX
+      const disY = nowY - params.currentY
+      console.log(disX, disY)
+      target.css('left', parseInt(params.left) + disX + 'px')
+      target.css('top', parseInt(params.top) + disY + 'px')
+    })
   }
-
 }
 </script>
 
